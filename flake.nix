5bc41ca0e900b59a3da5b8f80cad9b348ee5f5d2
UEDA Tetsutaro <tueda1207@gmail.com>
2026-08-07 14:31:55 +0000

    fix(nix): update vendorHash for aws-sdk-go-v2 bump

--- flake.nix
+++ flake.nix
@@ -23,7 +23,7 @@
           pname = "op-aws-credential-process";
           version = "0.1.1";
           src = ./.;
-          vendorHash = "sha256-aDA/XfeCjwqP+KlJ104XRQ5v1XChtXRtdBzBAoLzzWk=";
+          vendorHash = "sha256-Frx6RDQsepX6Vz+Xg5JkjWlghUgwwmzgoLJLFJG/Qmc=";
           ldflags = [
             "-s"
             "-w"
